1e6fe406ae259e8b424b3737049d25f2a052c988
Chris Lattman <natgeo13@gmail.com>
2024-04-24 20:43:34 -0400

    update

--- rust/local_client/local.rs
+++ rust/local_client/local.rs
@@ -1,14 +1,14 @@
 use std::{
-    fs::{self, OpenOptions},
-    io::{Read, Seek, SeekFrom, Write},
-    str,
+    fs::{metadata, remove_file, rename, OpenOptions},
+    io::{Read, Seek, SeekFrom::Start, Write},
+    str::from_utf8,
     time::SystemTime,
 };
 
 use chrono::{TimeZone, Utc};
 
 fn main() {
-    let stat = fs::metadata("test_file.txt").unwrap();
+    let stat = metadata("test_file.txt").unwrap();
     println!("File size (in bytes): {}", stat.len());
     let timestamp = stat
         .modified()
@@ -30,19 +30,19 @@ fn main() {
         .unwrap();
     let mut buf = [0u8; 5];
     let status = file.read(&mut buf).unwrap();
-    println!("{} {}", status, str::from_utf8(&buf).unwrap());
-    file.seek(SeekFrom::Start(0)).unwrap();
+    println!("{} {}", status, from_utf8(&buf).unwrap());
+    file.seek(Start(0)).unwrap();
     let mut buf2 = [0u8; 6];
     let status = file.read(&mut buf2).unwrap();
     println!(
         "{} {}{}",
         status,
-        str::from_utf8(&buf).unwrap(),
-        str::from_utf8(&buf2).unwrap()
+        from_utf8(&buf).unwrap(),
+        from_utf8(&buf2).unwrap()
     );
     file.write("word".as_bytes()).unwrap();
     file.sync_all().unwrap();
 
-    fs::rename("test_file.txt", "renamed_file.txt").unwrap();
-    fs::remove_file("deleted_file.txt").unwrap();
+    rename("test_file.txt", "renamed_file.txt").unwrap();
+    remove_file("deleted_file.txt").unwrap();
 }
